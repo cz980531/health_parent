8d5e4fb4ff789edf1212a2e557a7f570d0b8f2c5
cz <15734657675@163.com>
2021-01-06 16:47:42 +0800

    Initial commit

---
+++ health_web/src/main/java/com/itheima/health/controller/CheckItemController.java
@@ -0,0 +1,30 @@
+package com.itheima.health.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.itheima.health.constant.MessageConstant;
+import com.itheima.health.entity.Result;
+import com.itheima.health.pojo.CheckItem;
+import com.itheima.health.service.CheckItemService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/checkitem")
+public class CheckItemController {
+
+    @Reference
+    private CheckItemService checkItemService;
+
+    @GetMapping("/findAll")
+    public Result findAll(){
+     List<CheckItem> list = checkItemService.findAll();
+     return new Result(true, MessageConstant.QUERY_CHECKITEM_SUCCESS,list);
+    }
+
+    @PostMapping("/add")
+    public Result add(@RequestBody CheckItem checkItem){
+        checkItemService.add(checkItem);
+        return new Result(true,MessageConstant.ADD_CHECKITEM_SUCCESS);
+    }
+}
